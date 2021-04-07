016258499c3087584bb03aed71fe99fbcd112895
Francisco Guerrero <francisco.guerrero@rookmotion.com>
2021-04-07 17:40:33 -0500

    gitignore added

---
+++ testCocoa/testCocoa/SesionFuctions.swift
@@ -0,0 +1,8 @@
+//
+//  SesionFuctions.swift
+//  testCocoa
+//
+//  Created by Francisco Rook on 07/04/21.
+//
+
+import Foundation
---
+++ testCocoa/testCocoa/testCocoa.h
@@ -0,0 +1,18 @@
+//
+//  testCocoa.h
+//  testCocoa
+//
+//  Created by Francisco Rook on 07/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testCocoa.
+FOUNDATION_EXPORT double testCocoaVersionNumber;
+
+//! Project version string for testCocoa.
+FOUNDATION_EXPORT const unsigned char testCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testCocoa/PublicHeader.h>
+
+
