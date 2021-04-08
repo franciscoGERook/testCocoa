c91180169f57c82ae89e8ea69e883c8ffcd71465
Francisco Guerrero <francisco.guerrero@rookmotion.com>
2021-04-08 16:40:40 -0500

    testCocoa

--- testCocoa/testCocoa/SesionFuctions.swift
+++ testCocoa/testCocoa/SesionFuctions.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import Alamofire
+
+public class SesionFunctions {
+    private init() { }
+    
+    public static func add2Numbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    public static func printSomething() {
+        print("Test cocoapods")
+    }
+}
